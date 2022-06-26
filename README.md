aa6028a71645d4e5eeee711400e9afa1475d1dfa
IlyaKoch <kochetkov6363@gmail.com>
2022-06-26 23:24:39 +0400

    add test SelenideSearch

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 - Junit5
 - Selenide
 
-123
---
+++ src/test/java/github/SelenideSearch.java
@@ -0,0 +1,30 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideSearch {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void shouldFindSelenideJUnit5Code() {
+        open("https://github.com");
+        $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
+        var listOfRepositories = $$(".repo-list li");
+        listOfRepositories.first().$("a").click();
+        $("h1").should(text("selenide/selenide"));
+        $("#wiki-tab").click();
+//        var listOfPages =
+        $$(".markdown-body ul li").get(6).$("a").should(text("Soft assertions")).click();
+        var listOfMechanisms = $$(".markdown-body ol li");
+        listOfMechanisms.findBy(text("JUnit5 extension")).should(text("JUnit5"));
+    }
+}
